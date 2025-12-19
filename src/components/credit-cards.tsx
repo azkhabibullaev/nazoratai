b0618658c7d230786473c36fc786fd5cddfd0c4b
Azamat Khabibullaev <azkhabibullaev@gmail.com>
2025-12-19 17:39:42 +0500

    v2

--- src/components/credit-cards.tsx
+++ src/components/credit-cards.tsx
@@ -1,6 +1,5 @@
 import * as React from "react";
 import { Card, CardContent } from "@/components/ui/card";
-import { cn } from "@/lib/utils";
 import useEmblaCarousel from "embla-carousel-react";
 
 export type CreditCardDTO = {
