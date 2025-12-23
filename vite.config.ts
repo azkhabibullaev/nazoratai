754a8e1382178a9d9ce824060715b30fe3f053ef
Azamat Khabibullaev <azkhabibullaev@gmail.com>
2025-12-23 11:52:58 +0500

    v10

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,6 @@
 import path from "node:path";
-import { readFileSync } from "node:fs";
 import tailwindcss from "@tailwindcss/vite";
 import react from "@vitejs/plugin-react";
-import basicSsl from "@vitejs/plugin-basic-ssl";
 import { defineConfig } from "vite";
 import { tanstackRouter } from "@tanstack/router-plugin/vite";
 
@@ -11,7 +9,6 @@ const TARGET = "https://api.webhook.nazorat-ai.uz";
 // https://vite.dev/config/
 export default defineConfig({
     plugins: [
-        basicSsl(),
         tanstackRouter({
             target: "react",
             autoCodeSplitting: true,
@@ -25,11 +22,6 @@ export default defineConfig({
         },
     },
     server: {
-        host: true,
-        https: {
-            cert: readFileSync(path.resolve("tma.internal.pem")),
-            key: readFileSync(path.resolve("tma.internal-key.pem")),
-        },
         proxy: {
             "/api": {
                 target: TARGET,
