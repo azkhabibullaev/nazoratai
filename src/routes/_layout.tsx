f5b82b7e064ff94ca58b1b4950e2c307a9b25a81
Azamat Khabibullaev <azkhabibullaev@gmail.com>
2025-12-29 09:02:31 +0500

    v101

--- src/routes/_layout.tsx
+++ src/routes/_layout.tsx
@@ -3,6 +3,7 @@ import { AppDrawer } from "@/components/app-drawer/app-drawer";
 import { BottomNavigation } from "@/components/bottom-nav";
 import { Header } from "@/components/header";
 import { useVerifyTgTokenQuery } from "@/entities/session/verify/verify.query";
+import { useMeQuery } from "@/entities/session/me/me.query";
 
 export const Route = createFileRoute("/_layout")({
   validateSearch: (search: Record<string, unknown>) => {
@@ -18,12 +19,14 @@ function Layout() {
   //   const navigate = Route.useNavigate();
 
   const verify = useVerifyTgTokenQuery(token);
+  const userMe = useMeQuery();
 
   return (
     <div className="relative min-h-screen max-w-xl mx-auto px-4 pb-32 mt-20">
       <Header />
       <main>
         <pre>{JSON.stringify(verify.data, null, 2)}</pre>
+        <pre>{JSON.stringify(userMe.data, null, 2)}</pre>
         <Outlet />
       </main>
       <BottomNavigation />
