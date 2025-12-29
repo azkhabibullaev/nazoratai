31c72c3d6a2d00a97f4d97319ba418c088255774
Azamat Khabibullaev <azkhabibullaev@gmail.com>
2025-12-29 10:13:51 +0500

    v108

--- src/entities/session/me/me.query.ts
+++ src/entities/session/me/me.query.ts
@@ -1,14 +1,11 @@
 import { useQuery } from "@tanstack/react-query";
 import { api } from "@/shared/api/base";
-import { useVerifyStore } from "../verify/verify.store";
 import { useMeStore } from "./me.store";
 
 export function useMeQuery() {
-  const accessToken = useVerifyStore((s) => s.accessToken);
   const setMe = useMeStore((s) => s.setMe);
   return useQuery({
     queryKey: ["tg-user"],
-    enabled: Boolean(accessToken),
     queryFn: async () => {
       const response = await api.get("/users/getMe");
       const data = response.data?.data;
