f1451e1e4335b593f07243fdf28617db984aa592
Azamat Khabibullaev <azkhabibullaev@gmail.com>
2025-12-29 09:58:28 +0500

    v105

--- src/routes/_layout.tsx
+++ src/routes/_layout.tsx
@@ -5,6 +5,7 @@ import { Header } from "@/components/header";
 import { useVerifyTgTokenQuery } from "@/entities/session/verify/verify.query";
 import { useMeQuery } from "@/entities/session/me/me.query";
 import { useEffect } from "react";
+import { useVerifyStore } from "@/entities/session/verify/verify.store";
 
 export const Route = createFileRoute("/_layout")({
   validateSearch: (search: Record<string, unknown>) => {
@@ -18,22 +19,23 @@ export const Route = createFileRoute("/_layout")({
 function Layout() {
   const { token } = Route.useSearch();
   const navigate = Route.useNavigate();
+  const accessToken = useVerifyStore((s) => s.accessToken);
 
   const verify = useVerifyTgTokenQuery(token);
   const userMe = useMeQuery();
 
   useEffect(() => {
-	if (!token) return;
-	if (!verify.isSuccess) return
-	navigate({
-		to: "/",
-		replace: true,
-		search: (prev) => ({
-			...(prev as Record<string, unknown>),
-			token: undefined,
-		}),
-	});
-  }, [token, navigate]);
+	if (accessToken) {
+		navigate({
+			to: "/",
+			replace: true,
+			search: (prev) => ({
+				...(prev as Record<string, unknown>),
+				token: undefined,
+			}),
+		});
+	};
+  }, []);
 
   return (
     <div className="relative min-h-screen max-w-xl mx-auto px-4 pb-32 mt-20">
