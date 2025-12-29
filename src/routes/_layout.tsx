d7d23e82098ef4722450e93235b4338ac9bfeb80
Azamat Khabibullaev <azkhabibullaev@gmail.com>
2025-12-29 08:52:58 +0500

    v99

--- src/routes/_layout.tsx
+++ src/routes/_layout.tsx
@@ -2,9 +2,7 @@ import { createFileRoute, Outlet } from "@tanstack/react-router";
 import { AppDrawer } from "@/components/app-drawer/app-drawer";
 import { BottomNavigation } from "@/components/bottom-nav";
 import { Header } from "@/components/header";
-import { useEffect, useState } from "react";
-import { publicApi } from "@/shared/api/base";
-import axios from "axios";
+import { useVerifyTgTokenQuery } from "@/entities/session/verify/verify.query";
 
 export const Route = createFileRoute("/_layout")({
   validateSearch: (search: Record<string, unknown>) => {
@@ -19,51 +17,13 @@ function Layout() {
   const { token } = Route.useSearch();
   //   const navigate = Route.useNavigate();
 
-  const [data, setData] = useState(null);
-  const [loading, setLoading] = useState(true);
-  const [error, setError] = useState<string | null>(null);
-
-  useEffect(() => {
-    let isMounted = true;
-    async function verifyToken() {
-      try {
-        const response = await publicApi.get(`/users/getToken/${token}`);
-        const accessToken = response.data?.data?.accessToken;
-        if (isMounted) {
-          console.log("Access Token:", accessToken);
-          setData(response.data?.data?.accessToken);
-        }
-      } catch (err: unknown) {
-        if (!isMounted) return;
-        if (axios.isAxiosError(err)) {
-          setError(err.response?.data?.message ?? err.message);
-        } else if (err instanceof Error) {
-          setError(err.message);
-        } else {
-          setError("Unknown error");
-        }
-      } finally {
-        if (isMounted) {
-          setLoading(false);
-        }
-      }
-    }
-
-    verifyToken();
-
-    return () => {
-      isMounted = false;
-    };
-  }, [token]);
-
-  if (loading) return <p>Loading data...</p>;
-  if (error) return <p>Error: {error}</p>;
+  const verify = useVerifyTgTokenQuery(token);
 
   return (
     <div className="relative min-h-screen max-w-xl mx-auto px-4 pb-32 mt-20">
       <Header />
       <main>
-        <pre>{JSON.stringify(data, null, 2)}</pre>
+        <pre>{JSON.stringify(verify.data, null, 2)}</pre>
         <Outlet />
       </main>
       <BottomNavigation />
