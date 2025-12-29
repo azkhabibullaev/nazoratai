49f9baed161547d930418e5274de3b24c0fd4f12
Azamat Khabibullaev <azkhabibullaev@gmail.com>
2025-12-29 08:32:14 +0500

    v93

--- src/components/reports/reports.tsx
+++ src/components/reports/reports.tsx
@@ -11,83 +11,69 @@ import { ReportsList } from "./reports-list";
 import type { TPeriod } from "./types";
 
 export function Reports() {
-	const period = useReportsStore((s) => s.period);
-	const tab = useReportsStore((s) => s.tab);
-	const setPeriod = useReportsStore((s) => s.setPeriod);
-	const setTab = useReportsStore((s) => s.setTab);
+  const period = useReportsStore((s) => s.period);
+  const tab = useReportsStore((s) => s.tab);
+  const setPeriod = useReportsStore((s) => s.setPeriod);
+  const setTab = useReportsStore((s) => s.setTab);
 
-	const { data, isPending } = useReportsQuery(period);
+  const { data, isPending } = useReportsQuery(period);
 
-	return (
-		<div>
-			<div className="flex items-center justify-between gap-2 mb-2">
-				{isPending ? (
-					<>
-						<Skeleton className="flex-1 border h-[58px] w-[250px] rounded-xl" />
-						<Skeleton className="flex-1 border h-[58px] w-[250px] rounded-xl" />
-					</>
-				) : (
-					<>
-						<div className="flex-1 flex items-center justify-center gap-4 border rounded-xl p-2 text-red-400 bg-background">
-							<HugeiconsIcon
-								className="size-6"
-								icon={ChartDownIcon}
-							/>
-							<div className="flex flex-col text-sm font-medium">
-								<span>Xarajat</span>
-								<span>
-									-{formatCurrency(data?.totalExpense ?? 0)}
-								</span>
-							</div>
-						</div>
-						<div className="flex-1 flex items-center justify-center gap-4 border rounded-xl p-2 text-green-400 bg-background">
-							<HugeiconsIcon
-								className="size-6"
-								icon={ChartUpIcon}
-							/>
-							<div className="flex flex-col text-center text-sm font-medium">
-								<span>Daromad</span>
-								<span>
-									+{formatCurrency(data?.totalIncome ?? 0)}
-								</span>
-							</div>
-						</div>
-					</>
-				)}
-			</div>
-			<Tabs
-				value={period}
-				onValueChange={(v) => setPeriod(v as TPeriod)}
-				className="w-full mb-2"
-			>
-				<TabsList className="w-full">
-					<TabsTrigger value="today">Kunlik</TabsTrigger>
-					<TabsTrigger value="week">Haftalik</TabsTrigger>
-					<TabsTrigger value="month">Oylik</TabsTrigger>
-				</TabsList>
-			</Tabs>
-			<Card className="flex flex-col">
-				<CardContent className="flex-1 pb-0">
-					<ReportsChart
-						tab={tab}
-						reports={data}
-						loading={isPending}
-					/>
-				</CardContent>
-				<CardFooter className="flex-col gap-2 text-sm">
-					<Tabs
-						value={tab}
-						onValueChange={(v) => setTab(v as "expense" | "income")}
-						className="w-full my-2"
-					>
-						<TabsList className="w-full">
-							<TabsTrigger value="expense">Chiqim</TabsTrigger>
-							<TabsTrigger value="income">Kirim</TabsTrigger>
-						</TabsList>
-					</Tabs>
-					<ReportsList tab={tab} reports={data} loading={isPending} />
-				</CardFooter>
-			</Card>
-		</div>
-	);
+  return (
+    <div>
+      <div className="flex items-center justify-between gap-2 mb-2">
+        {isPending ? (
+          <>
+            <Skeleton className="flex-1 border h-14.5 w-62.5 rounded-xl" />
+            <Skeleton className="flex-1 border h-14.5 w-62.5 rounded-xl" />
+          </>
+        ) : (
+          <>
+            <div className="flex-1 flex items-center justify-center gap-4 border rounded-xl p-2 text-red-400 bg-background">
+              <HugeiconsIcon className="size-6" icon={ChartDownIcon} />
+              <div className="flex flex-col text-sm font-medium">
+                <span>Xarajat</span>
+                <span>-{formatCurrency(data?.totalExpense ?? 0)}</span>
+              </div>
+            </div>
+            <div className="flex-1 flex items-center justify-center gap-4 border rounded-xl p-2 text-green-400 bg-background">
+              <HugeiconsIcon className="size-6" icon={ChartUpIcon} />
+              <div className="flex flex-col text-center text-sm font-medium">
+                <span>Daromad</span>
+                <span>+{formatCurrency(data?.totalIncome ?? 0)}</span>
+              </div>
+            </div>
+          </>
+        )}
+      </div>
+      <Tabs
+        value={period}
+        onValueChange={(v) => setPeriod(v as TPeriod)}
+        className="w-full mb-2"
+      >
+        <TabsList className="w-full">
+          <TabsTrigger value="today">Kunlik</TabsTrigger>
+          <TabsTrigger value="week">Haftalik</TabsTrigger>
+          <TabsTrigger value="month">Oylik</TabsTrigger>
+        </TabsList>
+      </Tabs>
+      <Card className="flex flex-col">
+        <CardContent className="flex-1 pb-0">
+          <ReportsChart tab={tab} reports={data} loading={isPending} />
+        </CardContent>
+        <CardFooter className="flex-col gap-2 text-sm">
+          <Tabs
+            value={tab}
+            onValueChange={(v) => setTab(v as "expense" | "income")}
+            className="w-full my-2"
+          >
+            <TabsList className="w-full">
+              <TabsTrigger value="expense">Chiqim</TabsTrigger>
+              <TabsTrigger value="income">Kirim</TabsTrigger>
+            </TabsList>
+          </Tabs>
+          <ReportsList tab={tab} reports={data} loading={isPending} />
+        </CardFooter>
+      </Card>
+    </div>
+  );
 }
--- src/routes/_layout.tsx
+++ src/routes/_layout.tsx
@@ -1,62 +1,65 @@
 import { createFileRoute, Outlet } from "@tanstack/react-router";
-import axios from "axios";
-import { useEffect, useState } from "react";
 import { AppDrawer } from "@/components/app-drawer/app-drawer";
 import { BottomNavigation } from "@/components/bottom-nav";
 import { Header } from "@/components/header";
+import { useEffect, useState } from "react";
 import { publicApi } from "@/shared/api/base";
 
 export const Route = createFileRoute("/_layout")({
-	validateSearch: (search: Record<string, unknown>) => {
-		return {
-			token: typeof search.token === "string" ? search.token : undefined,
-		};
-	},
-	component: Layout,
+  validateSearch: (search: Record<string, unknown>) => {
+    return {
+      token: typeof search.token === "string" ? search.token : undefined,
+    };
+  },
+  component: Layout,
 });
 
 function Layout() {
-	const { token } = Route.useSearch();
-	const navigate = Route.useNavigate();
+  const { token } = Route.useSearch();
+  const navigate = Route.useNavigate();
 
-	const [loading, setLoading] = useState<boolean>(false);
-	const [errorMessage, setErrorMessage] = useState<string | null>(null);
-	const [accessToken, setAccessToken] = useState<string | undefined>();
+  const [data, setData] = useState(null);
+  const [loading, setLoading] = useState(true);
+  const [error, setError] = useState<string | unknown>("");
 
-	console.log(loading, errorMessage, accessToken, navigate);
+  useEffect(() => {
+    let isMounted = true;
+    async function verifyToken() {
+      try {
+        const response = await publicApi.get(`/users/getToken/${token}`);
+        if (isMounted) {
+          setData(response.data);
+        }
+      } catch (error) {
+        if (isMounted) {
+          setError(error);
+        }
+      } finally {
+        if (isMounted) {
+          setLoading(false);
+        }
+      }
+    }
 
-	useEffect(() => {
-		const controller = new AbortController();
-		(async () => {
-			setLoading(true);
-			setErrorMessage(null);
-			try {
-				const res = await publicApi.get(`/users/getToken/${token}`, {
-					signal: controller.signal,
-				});
-				setAccessToken(res.data.accessToken);
-			} catch (e) {
-				if (controller.signal.aborted) return;
-				if (axios.isAxiosError(e)) {
-					setErrorMessage(e.response?.data?.message ?? e.message);
-				} else {
-					setErrorMessage("Noma'lum xatolik");
-				}
-			} finally {
-				setLoading(false);
-			}
-		})();
-		return () => controller.abort();
-	}, [token]);
+    verifyToken();
 
-	return (
-		<div className="relative min-h-screen max-w-xl mx-auto px-4 pb-32 mt-20">
-			<Header />
-			<main>
-				<Outlet />
-			</main>
-			<BottomNavigation />
-			<AppDrawer />
-		</div>
-	);
+    return () => {
+      isMounted = false;
+    };
+  }, [token]);
+
+  if (loading) return <p>Loading data...</p>;
+  if (error) return <p>Error: {error.message}</p>;
+
+  return (
+    <div className="relative min-h-screen max-w-xl mx-auto px-4 pb-32 mt-20">
+      <Header />
+      <main>
+        <pre>{JSON.stringify(data, null, 2)}</pre>
+        <Outlet />
+      </main>
+      <BottomNavigation />
+      <AppDrawer />
+    </div>
+  );
 }
